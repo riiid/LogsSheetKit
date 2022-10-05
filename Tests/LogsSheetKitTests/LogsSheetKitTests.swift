a781062c15a423ee061f7b85f8344568d193c097
Yessen <y.yesen.y@gmail.com>
2022-10-05 23:20:58 +0900

    :tada: Begin project!

--- Tests/LogsSheetKitTests/LogsSheetKitTests.swift
+++ Tests/LogsSheetKitTests/LogsSheetKitTests.swift
@@ -6,6 +6,5 @@ final class LogsSheetKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LogsSheetKit().text, "Hello, World!")
     }
 }
